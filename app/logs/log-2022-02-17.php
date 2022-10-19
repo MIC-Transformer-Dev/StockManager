5dcd6be73cfcbe645139008643423bcadd82a277
AnthonyGunardi <anthonygunardi@gmail.com>
2022-10-19 11:09:53 +0700

    app is ready

---
+++ app/logs/log-2022-02-17.php
@@ -0,0 +1,26 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2022-02-17 11:17:57 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'dbuser'@'localhost' (using password: YES) C:\XAMPP\htdocs\stock-manager\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2022-02-17 11:17:57 --> Unable to connect to the database
+ERROR - 2022-02-17 11:17:57 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'dbuser'@'localhost' (using password: YES) C:\XAMPP\htdocs\stock-manager\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2022-02-17 11:17:57 --> Severity: Warning --> session_start(): Failed to initialize storage module: user (path: C:\XAMPP\tmp) C:\XAMPP\htdocs\stock-manager\system\libraries\Session\Session.php 143
+ERROR - 2022-02-17 11:17:57 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'dbuser'@'localhost' (using password: YES) C:\XAMPP\htdocs\stock-manager\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2022-02-17 11:17:57 --> Unable to connect to the database
+ERROR - 2022-02-17 11:17:57 --> Query error: Access denied for user 'dbuser'@'localhost' (using password: YES) - Invalid query: SET SESSION sql_mode = ""
+ERROR - 2022-02-17 11:17:57 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'dbuser'@'localhost' (using password: YES) C:\XAMPP\htdocs\stock-manager\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2022-02-17 11:17:57 --> Unable to connect to the database
+ERROR - 2022-02-17 11:17:57 --> Query error: Access denied for user 'dbuser'@'localhost' (using password: YES) - Invalid query: SELECT *
+FROM `sma_settings`
+ERROR - 2022-02-17 11:17:57 --> Severity: error --> Exception: Call to a member function num_rows() on bool C:\XAMPP\htdocs\stock-manager\app\models\Site.php 305
+ERROR - 2022-02-17 11:18:04 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'dbuser'@'localhost' (using password: YES) C:\XAMPP\htdocs\stock-manager\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2022-02-17 11:18:04 --> Unable to connect to the database
+ERROR - 2022-02-17 11:18:04 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'dbuser'@'localhost' (using password: YES) C:\XAMPP\htdocs\stock-manager\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2022-02-17 11:18:04 --> Severity: Warning --> session_start(): Failed to initialize storage module: user (path: C:\XAMPP\tmp) C:\XAMPP\htdocs\stock-manager\system\libraries\Session\Session.php 143
+ERROR - 2022-02-17 11:18:04 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'dbuser'@'localhost' (using password: YES) C:\XAMPP\htdocs\stock-manager\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2022-02-17 11:18:04 --> Unable to connect to the database
+ERROR - 2022-02-17 11:18:04 --> Query error: Access denied for user 'dbuser'@'localhost' (using password: YES) - Invalid query: SET SESSION sql_mode = ""
+ERROR - 2022-02-17 11:18:04 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'dbuser'@'localhost' (using password: YES) C:\XAMPP\htdocs\stock-manager\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2022-02-17 11:18:04 --> Unable to connect to the database
+ERROR - 2022-02-17 11:18:04 --> Query error: Access denied for user 'dbuser'@'localhost' (using password: YES) - Invalid query: SELECT *
+FROM `sma_settings`
+ERROR - 2022-02-17 11:18:04 --> Severity: error --> Exception: Call to a member function num_rows() on bool C:\XAMPP\htdocs\stock-manager\app\models\Site.php 305
